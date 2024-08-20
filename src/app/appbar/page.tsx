4ad41dfd8fa546acaf4998c1b3d64ceed3fee7b6
Jesse Cruickshank <jcru0005@student.monash.edu>
2024-08-20 16:11:53 +1000

    fix: Remove Navigation from Login and Register

--- src/app/appbar/page.tsx
+++ src/app/appbar/page.tsx
@@ -17,13 +17,22 @@ import {
     Divider,
     Drawer,
 } from "@mui/material";
+import { usePathname, useRouter } from "next/navigation";
+import path from "path";
 
 export default function ButtonAppBar() {
     const [open, setOpen] = React.useState(false);
+    const router = useRouter();
+    const [shouldShowIconButton, setShouldShowIconButton] = React.useState(true);
 
     const toggleDrawer = (newOpen: boolean) => () => {
         setOpen(newOpen);
     };
+    const pathname = usePathname();
+    React.useEffect(() => {
+        const pathsWithoutIconButton = ["/login", "/register"];
+        setShouldShowIconButton(!pathsWithoutIconButton.includes(pathname));
+    }, [pathname]);
 
     const DrawerList = (
         <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
@@ -67,20 +76,24 @@ export default function ButtonAppBar() {
         </Box>
     );
 
+    // const shouldShowIconButton = !["/login", "/register"].includes(location.pathname);
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        onClick={toggleDrawer(true)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+                    {shouldShowIconButton && (
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                            onClick={toggleDrawer(true)}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                    )}
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Easy Task
                     </Typography>
