b6c339410dfdb9c830f028fd55d227fa2bde3a61
Chongjie <cche0204@student.monash.edu>
2024-09-05 13:44:23 +1000

    add e2e navigation tests add e2e navigation tests for dashboard, login and register pages. Also added authentication for all tests such that they start off logged in.

--- tests/e2e/navigation.spec.ts
+++ tests/e2e/navigation.spec.ts
@@ -1,19 +1,33 @@
 import { test, expect } from "@playwright/test";
 
 test("should navigate correctly", async ({ page }) => {
+    // start tests in authenticated state
+    await page.goto("http://localhost:3000/dashboard");
+
+    // dashboard buttons should open another page calendar view, list view, etc.
+    await page.getByRole("button", { name: "CALENDAR VIEW" }).click();
+    await expect(page).toHaveURL("http://localhost:3000/calendar-view");
+    await page.goto("http://localhost:3000/dashboard");
+
+    await page.getByRole("button", { name: "LIST VIEW" }).click();
+    await expect(page).toHaveURL("http://localhost:3000/list-view");
+    await page.goto("http://localhost:3000/dashboard");
+
+    await page.getByRole("button", { name: "KANBAN VIEW" }).click();
+    await expect(page).toHaveURL("http://localhost:3000/kanban-view");
+    await page.goto("http://localhost:3000/dashboard");
+
+    const appBar = page.locator("nav.appbar");
+    await expect(appBar).toBeVisible();
+
+    await page.goto("http://localhost:3000/logout");
     // expect to be able to go to register page
     await page.goto("http://localhost:3000/register");
     await expect(page).toHaveURL("http://localhost:3000/register");
 
-    // on the register page, we expect to be able to register for an account and submit form
-    // we expect to have 4 textboxes for email, password, first name and last name
-    await expect(page.getByRole("textbox")).toHaveCount(4);
-
     // expect to be able to go to login page
     await page.goto("http://localhost:3000/login");
     await expect(page).toHaveURL("http://localhost:3000/login");
-    // expect to have email and password forms
-    await expect(page.getByRole("textbox")).toHaveCount(2);
 
     // expect to not be able to go to dashboard without logging in
     await page.goto("http://localhost:3000/dashboard");
