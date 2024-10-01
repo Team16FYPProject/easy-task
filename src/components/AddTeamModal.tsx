d0d56bc61ad88acc02755385d3127877d94781c4
Chongjie <chongjiechen@outlook.com>
2024-10-01 21:55:02 +1000

    fix: fix heights to be responsive

--- src/components/AddTeamModal.tsx
+++ src/components/AddTeamModal.tsx
@@ -6,15 +6,17 @@ import Modal from "@mui/material/Modal";
 import { Grid, TextField } from "@mui/material";
 
 const style = {
-    position: "absolute" as "absolute",
+    position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: "75%",
+    height: "75%",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
+    overflow: "auto",
 };
 
 export default function AddTeamModal({
