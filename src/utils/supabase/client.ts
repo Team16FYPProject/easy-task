16b0e6aa1a9906df36e9e5ef0f26bb3a52c8f0a9
ElijahAA <45959516+ElijahAA@users.noreply.github.com>
2024-10-09 23:31:13 +1100

    Attempt #9 to fix E2E action.

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,7 +3,7 @@ import { Database } from "@/utils/supabase/database.types";
 
 export function getBrowserSupabase() {
     return createBrowserClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        process.env.NEXT_PUBLIC_SUPABASE_URL ?? "http://127.0.0.1:54321/",
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "SUPABASE_ANON_KEY",
     );
 }
